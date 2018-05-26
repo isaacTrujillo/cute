35f4722ef5d41881bcb31d67be5d5cc3cf63cbc4
isaacTrujillo <isaac_filos2000@outlook.com>
2018-05-26 06:28:28 -0500

    base structure for frotend

--- AC/ac.rb
+++ AC/ac.rb
@@ -10,10 +10,19 @@ require_relative 'routes/users'
 require_relative 'controller/user'
 require_relative 'routes/chats'
 require_relative 'controller/chat'
+require_relative 'routes/contest_users'
+require_relative 'controller/contest_user'
+require_relative 'routes/problems'
+require_relative 'controller/problem'
+require_relative 'routes/contest_problems'
+require_relative 'controller/contest_problem'
 
 class AC < Grape::API
 
   Controller::Contest.register(:database, Services.database[:contest])
+  Controller::ContestUser.register(:database, Services.database[:contest_has_user])
+  Controller::ContestProblem.register(:database, Services.database[:contest_has_problem])
+  Controller::Problem.register(:database, Services.database[:problem])
   Controller::User.register(:database, Services.database[:user])
   Controller::Chat.register(:database, Services.database[:chat])
 
@@ -25,4 +34,7 @@ class AC < Grape::API
   mount Routes::Contests
   mount Routes::Users
   mount Routes::Chats
+  mount Routes::ContestUsers
+  mount Routes::ContestProblems
+  mount Routes::Problems
 end
